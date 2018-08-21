8823ab2e5c7785d7b884b51fc1721afd6cc9f576
Raghavan R <raghavan.r@capgemini.com>
2018-08-21 14:17:28 +1000

    adding rds tf files

---
+++ rds.tf
@@ -0,0 +1,10 @@
+resource "aws_db_instance" "rds" {
+  allocated_storage    = "${var.allocated_storage}"
+  storage_type         = "${var.storage_type}"
+  engine               = "${var.engine}"
+  engine_version       = "${var.engine_version}"
+  instance_class       = "${var.instance_class}"
+  name                 = "${var.name}"
+  username             = "${var.username}"
+  password             = "${var.password}"
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "allocated_storage" {}
+
+variable "storage_type" {}
+
+variable "engine" {}
+
+variable "engine_version" {}
+
+variable "instance_class" {}
+
+variable "name" {}
+
+variable "username" {}
+
+variable "password" {}
+
